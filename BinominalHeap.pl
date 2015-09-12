7a81916ba33c070c628bdcc3051699ec09c6a47c
maxyor6040 <maxyor6040@gmail.com>
2015-09-12 13:51:06 +0300

    Update BinominalHeap.pl
    day 2

--- BinominalHeap.pl
+++ BinominalHeap.pl
@@ -1,49 +1,54 @@
 
 
-{/Binomial tree node/}
+/* Binomial tree node */
+node(nil).
 node(Key,FatherNode,ChildrenNodeList).
 
-{/Binomial heap/}
+/* Binomial heap */
 bh([]).
 bh([node(_,_,_)|Rest]).
-emptyBH(NewBH):- NewBH is bh([]).
+emptyBH([]).
 
-{/tree depth calculation/}
+/* tree depth calculation */
 depth(node(_,_,[]),1).
-depth(node(_,_,[LeftChild|Rest]),Val):- depth(LeftChild,X), Val is 1+X.
+depth(node(_,_,[LeftChild|Rest]),Val):- depth(LeftChild,LeftChildDepth), Val is 1+LeftChildDepth.
 
-
-{/balancing a BH/}
+/* balancing a BH  - first 'arg' is input and second is output*/
 balanceBH([],[]).
+
 balanceBH([node(A,B,C)],[node(A,B,C)]).
 
-balanceBH([Big,Small|Rest],[Big,Small|Rest]):- 
-	depth(Small,D1),depth(Big,D2), D1=/=D2,
+balanceBH([Big,Small|Rest],Res):- 
+	depth(Big,Db),
+	depth(Small,Ds),
+	Ds =\= Db,
+	balanceBH([Small|Rest],Temp) ,
+	Res = [Big|Temp].
 	
 
 balanceBH([Big,Small|Rest],Res):- 
 	depth(Small,D),depth(Big,D),
-	Small is node(Key1,nil,Tail1), 
-	Big is node(Key2,nil,Tail2), 
+	Small = node(Key1,nil,Tail1), 
+	Big = node(Key2,nil,Tail2), 
 	key1<key2,
-	X is node(key1,nil,[node(key2,X,Tail2)|Tail1]),
+	X = node(key1,nil,[node(key2,X,Tail2)|Tail1]),
 	balanceBH([X|Rest], Res).
 
 
 balanceBH([Small,Big|Rest],Res):- 
 	depth(Small,D),depth(Big,D),
-	Small is node(Key1,nil,Tail1), 
-	Big is node(Key2,nil,Tail2), 
+	Small = node(Key1,nil,Tail1), 
+	Big = node(Key2,nil,Tail2), 
 	key1<key2,
-	X is node(key1,nil,[node(key2,X,Tail2)|Tail1]),
+	X = node(key1,nil,[node(key2,X,Tail2)|Tail1]),
 	balanceBH([X|Rest], Res).
 
 
-
+/* add */
 add(What, [], [node(What,nil,[])]).
 add(What, [First|Rest], AfterBH):- 
 	depth(First)>1,
-	AfterBH is [node(What,nil,[]),First|Rest].
+	AfterBH = [node(What,nil,[]),First|Rest].
 add(What, [First|Rest], AfterBH):- 
 	depth(First)=:=1,
 	balanceBH([node(What,nil,[]),First|Rest],AfterBH).
@@ -54,18 +59,7 @@ add(What, [First|Rest], AfterBH):-
 
 
 
-{/
-balanceBH([Small,Big],Res):- 
-	Small is node(Key1,nil,Tail1), 
-	Big is node(Key2,nil,Tail2), 
-	key1<key2,
-	Res is [X],
-	X is node(key1,nil,[node(key2,X,Tail2)|Tail1]).
-
-balanceBH([Big,Small],Res):- 
-	Small is node(Key1,nil,Tail1), 
-	Big is node(Key2,nil,Tail2), 
-	key1<key2,
-	Res is [X],
-	X is node(key1,nil,[node(key2,X,Tail2)|Tail1]).
-/}
+/**
+* emptyBH(X),add(2,X,Y),add(3,Y,Z),add(1,Z,A).
+* 
+*/
